3a0c03925e23caa51b6f253ede70a87370f25c9a
mirek <m.duraj2@wp.pl>
2021-09-13 06:13:09 +0200

    adding port

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.get('/', (req, res) => {
     res.send('Wrong address...Try port 3000');
 })
 
-app.listen(3001, () => {
-    console.log('app is running on port 3001');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
 // SIGNIN
